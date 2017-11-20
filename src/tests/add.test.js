334a529c5717a93f36c509decbdaac501216ed96
Unknown <magicdustin14@gmail.com>
2017-11-20 15:04:38 -0800

    Setup some example test cases

--- src/tests/add.test.js
+++ src/tests/add.test.js
@@ -1,6 +1,17 @@
 const add = (a,b) => a + b;
+const generateGreeting = (name = "Anonymous") => `Hello ${name}!`;
 
 test('Should add two numbers', () => {
   const result = add(3, 4);
   expect(result).toBe(7);
+});
+
+test('Should return a greeting with the name passed in', () => {
+  const result = generateGreeting('Dustin');
+  expect(result).toBe('Hello Dustin!');
+});
+
+test('Should return a greeting with no name', () => {
+  const result = generateGreeting();
+  expect(result).toBe('Hello Anonymous!');
 });
