0421de86f0bd18d784661bb1948235208fdaeeab
Unknown <magicdustin14@gmail.com>
2017-11-21 17:50:02 -0800

    Setup ExpenseList and ExpenseListItem Jest

--- src/tests/components/Header.test.js
+++ src/tests/components/Header.test.js
@@ -5,9 +5,4 @@ import Header from '../../components/Header';
 test('Should render header correctly', () => {
   const wrapper = shallow(<Header />);
   expect(wrapper).toMatchSnapshot();
-
-  //expect(wrapper.find('h1').text()).toBe('Expensify');
-  // const renderer = new reactShallowRenderer();
-  // renderer.render(<Header />);
-  // expect(renderer.getRenderOutput()).toMatchSnapshot();
 });
