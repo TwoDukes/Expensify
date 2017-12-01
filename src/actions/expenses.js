4d06f4274c2ec98343ce30293f6b14a6d31f3433
Unknown <magicdustin14@gmail.com>
2017-11-30 20:33:05 -0800

    Added a comment to expenses.js

--- src/actions/expenses.js
+++ src/actions/expenses.js
@@ -18,7 +18,7 @@ export const startAddExpense = (expenseData) => {
     } = expenseData
 
     const expense = { description, note, amount, createdAt};
-    
+    //Send new expense to firebase, then dispatch to local store
     database.ref('expenses').push(expense).then((ref) => {
       dispatch(addExpense({
         id: ref.key,
