3e8c23f78dd7c7c51d4d94e4f08a34614cc676f2
Unknown <magicdustin14@gmail.com>
2017-12-01 23:53:05 -0800

    Setup secondary database for testing purposes

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -2,12 +2,12 @@
   
   // Initialize Firebase
   var config = {
-    apiKey: "AIzaSyD2kY1yS25yOT_Kqj1xpTFb5tO6ztpjA8U",
-    authDomain: "expensify-b2b3f.firebaseapp.com",
-    databaseURL: "https://expensify-b2b3f.firebaseio.com",
-    projectId: "expensify-b2b3f",
-    storageBucket: "expensify-b2b3f.appspot.com",
-    messagingSenderId: "995457296426"
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.FIREBASE_DATABASE_URL,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
   };
 
   firebase.initializeApp(config);
