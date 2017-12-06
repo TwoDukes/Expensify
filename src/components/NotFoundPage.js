b65458f647c7a0b4c56fc9ea64c5fed0054dffbc
Unknown <magicdustin14@gmail.com>
2017-12-06 13:52:17 -0800

    Styled the 404 page

--- src/components/NotFoundPage.js
+++ src/components/NotFoundPage.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
+import { Header } from './Header';
 
 export const NotFoundPage = () => (
   <div>
-    404 - <Link to="/">Go Home</Link>
+    <div className='page-header'>
+      <div className='content-container'>
+        <h1 className='page-header__title'>404 - Page not found</h1>
+        <Link className='button' to="/">Go Home</Link>
+      </div>
+    </div>
   </div>
 );
 
--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -4,7 +4,6 @@ import AddExpensePage from '../components/AddExpensePage';
 import createHistory from 'history/createBrowserHistory';
 import EditExpensePage from '../components/EditExpensePage';
 import ExpenseDashboardPage from '../components/ExpenseDashboardPage';
-import Header from '../components/Header';
 import HelpPage from '../components/HelpPage';
 import NotFoundPage from '../components/NotFoundPage';
 import LoginPage from '../components/LoginPage';
@@ -21,8 +20,7 @@ const AppRouter = () => (
         <PrivateRoute path="/dashboard" component={ExpenseDashboardPage}/>        
         <PrivateRoute path="/create" component={AddExpensePage}/>
         <PrivateRoute path="/edit/:id" component={EditExpensePage}/>
-        <Route path="/help" component={HelpPage}/>
-        <Route component={NotFoundPage}/>
+        <PrivateRoute component={NotFoundPage}/>
       </Switch>   
     </div>
   </Router>
