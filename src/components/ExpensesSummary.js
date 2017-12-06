4e750a65b75983b53314842f5c19108cb46bfa38
Unknown <magicdustin14@gmail.com>
2017-12-06 14:06:47 -0800

    ExpenseSummary now shows if expenses are hidden by filters

--- src/components/ExpensesSummary.js
+++ src/components/ExpensesSummary.js
@@ -5,13 +5,21 @@ import numeral from 'numeral';
 import selectExpensesTotal from '../selectors/expenses-total';
 import selectExpenses from '../selectors/expenses';
 
-export const ExpensesSummary = ({expenseCount, expenseTotal}) => {
-  const expenseWord = expenseCount === 1 ? 'expense' : 'expenses';
+export const ExpensesSummary = ({expenseCount, expenseTotal, hiddenExpenses}) => {
+  const visibleExpenseWord = expenseCount === 1 ? 'expense' : 'expenses';
+  const hiddenExpenseWord = hiddenExpenses === 1 ? 'expense' : 'expenses';
   const formatedExpensesTotal = numeral(expenseTotal / 100).format('$0,0.00');
   return (
   <div className='page-header'>
     <div className="content-container">
-      <h1 className="page-header__title">Viewing <span>{expenseCount}</span> {expenseWord} totalling <span>{formatedExpensesTotal}</span></h1>
+      <h1 className="page-header__title">Viewing <span>{expenseCount}</span> {visibleExpenseWord} totalling <span>{formatedExpensesTotal}</span></h1>
+      {
+        hiddenExpenses > 0 ? (
+          <span className='page-header__subtitle'>{hiddenExpenses} {hiddenExpenseWord} are hidden</span>
+        ) : (
+          <span className='page-header__subtitle'>All Expenses Visible</span>
+        )
+      }
       <div className='page-header__actions'>
         <Link className='button' to='/create'>Add Expense</Link>
       </div>
@@ -25,7 +33,8 @@ const mapStateToProps = (state) => {
 
   return  {
     expenseCount: visibleExpenses.length,
-    expenseTotal: selectExpensesTotal(visibleExpenses)
+    expenseTotal: selectExpensesTotal(visibleExpenses),
+    hiddenExpenses: state.expenses.length - visibleExpenses.length
   };
 };
 
